d4480115cabe4e57aa3647cb82c4cb78694dcfdb
egorka_angelskiy <88832718+egorka-angelskiy@users.noreply.github.com>
2022-07-19 11:27:38 +0300

    Update vector.cpp

--- Lab/vector.cpp
+++ Lab/vector.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+#include <math.h>
 
+class Vector
+{
+	public:
+
+		Vector();
+		~Vector();
+};
+
+int main()
+{
+	std::cout << "hi vector" << std::endl;
+	std::cout << std::endl;
+
+	return 0;
+}
